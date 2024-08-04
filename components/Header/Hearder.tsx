0d361bad16958f451449917468d9431dfa77b7e3
diascw <wanessadiascw@gmail.com>
2024-08-04 20:26:14 -0300

    atualizando repo

---
+++ components/Header/Hearder.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-black text-white p-4 text-center">
+      <h1 className="text-3xl font-bold">Meu Blog de Receitas</h1>
+    </header>
+  );
+};
+
+export default Header;

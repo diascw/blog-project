7aa43802fbfbeda8030157a1c2113d0a0ccc5789
diascw <wanessadiascw@gmail.com>
2024-10-10 23:25:44 -0300

    atualizando a estrutura

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,16 +1,17 @@
-import { Post } from '../Post/Post';
+import { FC } from 'react';
 import { Post as PostType } from '../../types';
+import { Post } from '../Post/Post';
 
 interface FeedProps {
   posts: PostType[];
 }
 
-export function Feed({ posts }: FeedProps) {
+export const Feed: FC<FeedProps> = ({ posts }) => {
   return (
-    <>
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
+    <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {posts.map((post) => (
+        <Post key={post.title} post={post} />
       ))}
-    </>
+    </div>
   );
-}
+};
